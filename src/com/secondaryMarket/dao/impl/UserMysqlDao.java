0501e5db0d9d980d1405e1c8113d38278b25963c
Cuishibing <643237029@qq.com>
2016-04-19 23:03:41 +0800

    实现了getUserInId方法

--- src/com/secondaryMarket/dao/impl/UserMysqlDao.java
+++ src/com/secondaryMarket/dao/impl/UserMysqlDao.java
@@ -1,18 +1,48 @@
 package com.secondaryMarket.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.secondaryMarket.bean.User;
 import com.secondaryMarket.dao.UserDao;
+import com.secondaryMarket.factory.ConnectionFactory;
 
 public class UserMysqlDao implements UserDao{
-
+	private Connection connection = ConnectionFactory.createMySqlConnectionBuilder().getConnection();
 	@Override
 	public User getUserInId(Integer userId) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from user where userId=?";
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setInt(1, userId);
+			ResultSet rs = ps.executeQuery();
+			rs.last();
+			if(rs.getRow()<1){
+				return null;
+			}else{
+				rs.first();
+				User user = new User();
+				user.setUserEmail(rs.getString("userEmail"));
+				user.setUserId(userId);
+				user.setUserNackName(rs.getString("userNackName"));
+				user.setUserPassword(rs.getString("userPassword"));
+				user.setUserQQ(rs.getString("userQQ"));
+				user.setUserRealName(rs.getString("userRealName"));
+				user.setUserRole(rs.getString("userRole"));
+				user.setUserSchool(rs.getString("userSchool"));
+				user.setUserTel(rs.getString("userTel"));
+				return user;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override
-	public User getUserInName(String pickName) {
+	public User getUserInName(String realName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -24,7 +54,7 @@ public class UserMysqlDao implements UserDao{
 	}
 
 	@Override
-	public boolean validateUserName(String pickName) {
+	public boolean validateUserName(String realName) {
 		// TODO Auto-generated method stub
 		return false;
 	}
