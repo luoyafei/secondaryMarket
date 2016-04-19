445cde797860c1f3ee72b0f97ae862a378b42902
Cuishibing <643237029@qq.com>
2016-04-19 23:02:45 +0800

    编写了Commodity实体类

--- src/com/secondaryMarket/bean/Commodity.java
+++ src/com/secondaryMarket/bean/Commodity.java
@@ -1,5 +1,89 @@
 package com.secondaryMarket.bean;
 
 public class Commodity {
+	private Integer commodityId = -1;
+	private String commodityName = "";
+	private String commodityCategary = "";
+	private String commodityStatus = "";
+	private String commodityPicture = "";
+	private String commodityDescribe = "";
+	private Integer commodityCount = -1;
+	private Integer commodityOldNewLevel = -1;
+	private Integer commodityOldPrice = -1;
+	private Integer commodityNewPrice = -1;
+	private String commodityOwner = "";
+	private Integer commodityDownDay = -1;
 	
+	public Integer getCommodityId() {
+		return commodityId;
+	}
+	public void setCommodityId(Integer commodityId) {
+		this.commodityId = commodityId;
+	}
+	public String getCommodityName() {
+		return commodityName;
+	}
+	public void setCommodityName(String commodityName) {
+		this.commodityName = commodityName;
+	}
+	public String getCommodityCategary() {
+		return commodityCategary;
+	}
+	public void setCommodityCategary(String commodityCategary) {
+		this.commodityCategary = commodityCategary;
+	}
+	public String getCommodityStatus() {
+		return commodityStatus;
+	}
+	public void setCommodityStatus(String commodityStatus) {
+		this.commodityStatus = commodityStatus;
+	}
+	public String getCommodityPicture() {
+		return commodityPicture;
+	}
+	public void setCommodityPicture(String commodityPicture) {
+		this.commodityPicture = commodityPicture;
+	}
+	public String getCommodityDescribe() {
+		return commodityDescribe;
+	}
+	public void setCommodityDescribe(String commodityDescribe) {
+		this.commodityDescribe = commodityDescribe;
+	}
+	public Integer getCommodityCount() {
+		return commodityCount;
+	}
+	public void setCommodityCount(Integer commodityCount) {
+		this.commodityCount = commodityCount;
+	}
+	public Integer getCommodityOldNewLevel() {
+		return commodityOldNewLevel;
+	}
+	public void setCommodityOldNewLevel(Integer commodityOldNewLevel) {
+		this.commodityOldNewLevel = commodityOldNewLevel;
+	}
+	public Integer getCommodityOldPrice() {
+		return commodityOldPrice;
+	}
+	public void setCommodityOldPrice(Integer commodityOldPrice) {
+		this.commodityOldPrice = commodityOldPrice;
+	}
+	public Integer getCommodityNewPrice() {
+		return commodityNewPrice;
+	}
+	public void setCommodityNewPrice(Integer commodityNewPrice) {
+		this.commodityNewPrice = commodityNewPrice;
+	}
+	public String getCommodityOwner() {
+		return commodityOwner;
+	}
+	public void setCommodityOwner(String commodityOwner) {
+		this.commodityOwner = commodityOwner;
+	}
+	public Integer getCommodityDownDay() {
+		return commodityDownDay;
+	}
+	public void setCommodityDownDay(Integer commodityDownDay) {
+		this.commodityDownDay = commodityDownDay;
+	}
 }
