f6fa0524061df8c694105b20494feba8237f915e
Cuishibing <643237029@qq.com>
2016-04-19 21:52:05 +0800

    编写User实体类

--- src/com/secondaryMarket/bean/User.java
+++ src/com/secondaryMarket/bean/User.java
@@ -1,5 +1,67 @@
 package com.secondaryMarket.bean;
 
 public class User {
-	
+	private Integer userId = -1;
+	private String userNackName = "";
+	private String userPassword = "";
+	private String userRealName = "";
+	private String userTel = "";
+	private String userQQ = "";
+	private String userEmail = "";
+	private String userSchool = "";
+	private String userRole = "";
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserNackName() {
+		return userNackName;
+	}
+	public void setUserNackName(String userNackName) {
+		this.userNackName = userNackName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserRealName() {
+		return userRealName;
+	}
+	public void setUserRealName(String userRealName) {
+		this.userRealName = userRealName;
+	}
+	public String getUserTel() {
+		return userTel;
+	}
+	public void setUserTel(String userTel) {
+		this.userTel = userTel;
+	}
+	public String getUserQQ() {
+		return userQQ;
+	}
+	public void setUserQQ(String userQQ) {
+		this.userQQ = userQQ;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserSchool() {
+		return userSchool;
+	}
+	public void setUserSchool(String userSchool) {
+		this.userSchool = userSchool;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
 }
